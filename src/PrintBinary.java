56145b21265cbc76868a223cc65f942313922044
lmatos1 <32521826+lmatos1@users.noreply.github.com>
2017-11-09 21:44:48 -0800

    ..

--- src/PrintBinary.java
+++ src/PrintBinary.java
@@ -2,9 +2,20 @@ public class PrintBinary {
 
     public static void printBinary(int num){
         if(num >1){
-            int rem = num/2;
+            int rem = num%2;
             printBinary(num/2);
-            System.out.print(rem);
+            System.out.print(rem+" ");
         }
+        else {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int num;
+        System.out.print("Enter positive number: ");
+        num=StdIn.readInt();
+        System.out.print("Binary representation of "+num+" is: ");
+        printBinary(num);
     }
 }
